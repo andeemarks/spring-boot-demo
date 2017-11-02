fd57d41bee75b6c24ff2e3cf479cf6512b9a1b26
Andy Marks <vampwillow@gmail.com>
2017-11-02 21:59:06 +1100

    Now using embedded Jetty (thanks: https://medium.com/@yc/use-jetty9-with-spring-boot-and-gradle-85a1127c919f)

--- src/main/java/org/corvine/demo/DemoApplication.java
+++ src/main/java/org/corvine/demo/DemoApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
 
@@ -16,7 +15,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-    @Bean
+    //    @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
 
