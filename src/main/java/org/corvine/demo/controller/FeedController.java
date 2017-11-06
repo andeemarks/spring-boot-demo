ff8a1ed5cfc2ccfffc09a74fc7c0e933fa5c66c1
Andy Marks <vampwillow@gmail.com>
2017-11-06 11:02:37 +1100

    Got the beginnings of a Swoppy feed reader for /feed.  Connectivity is sketchy though - might need to add a cache next

---
+++ src/main/java/org/corvine/demo/controller/FeedController.java
@@ -0,0 +1,39 @@
+package org.corvine.demo.controller;
+
+import com.rometools.rome.feed.synd.SyndEntry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.PollableChannel;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class FeedController {
+    private static final Logger log = LoggerFactory.getLogger(FeedController.class);
+
+    @RequestMapping("/feed")
+    public String index() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("/feed/swoppy.xml");
+
+        PollableChannel feedChannel = context.getBean("swoppyFeed", PollableChannel.class);
+
+        Message<SyndEntry> message;
+        List<String> items = new ArrayList<>();
+        do {
+            message = (Message<SyndEntry>) feedChannel.receive(3000);
+            if (message != null) {
+                SyndEntry entry = message.getPayload();
+                items.add(entry.getTitle());
+            }
+        } while (message != null);
+
+        return items.toString();
+    }
+
+}
