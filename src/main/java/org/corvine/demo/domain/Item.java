de9d846c94c72a8a4fec9429e199ea779def5c52
Andy Marks <vampwillow@gmail.com>
2017-11-14 22:31:25 +1100

    Now parsing embedded HTML and pipe formatting in title and description feed fields

--- src/main/java/org/corvine/demo/domain/Item.java
+++ src/main/java/org/corvine/demo/domain/Item.java
@@ -7,8 +7,8 @@ import java.util.regex.Pattern;
 
 @Data
 public class Item {
+    private String id;
     private String title;
-    private String description;
     private String link;
     private String category;
     private String blurb;
@@ -19,22 +19,54 @@ public class Item {
     }
 
     public Item(String title, String description, String link) {
-
-        this.title = title;
-        this.description = description;
         this.link = link;
+        parseDescriptionComponents(description);
+        parseTitleComponents(title);
+    }
 
-        Pattern p = Pattern.compile("<p>Filed under: (.*)</p><p>(.*)<br />Price: (.*)<a href=\"(.*)\"><br />Read more ...</a></p>");
-        Matcher m = p.matcher(description);
+    private void parseTitleComponents(String title) {
+        Pattern titleRE = Pattern.compile("(MSS\\s[\\d]*)\\s\\|\\s(.*)");
+        Matcher m = titleRE.matcher(title);
+        if (!m.matches()) {
+            throw new IllegalArgumentException(title + " cannot be parsed");
+        }
+        this.title = m.group(2);
+        this.id = m.group(1);
+    }
+
+    private void parseDescriptionComponents(String description) {
+        Pattern descriptionRE = Pattern.compile("<p>Filed under: (.*)<\\/p><p>(.*)<br \\/>Price: (.*)<a href=\"(.*)\"><br \\/>Read more ...<\\/a><\\/p>");
+        Matcher m = descriptionRE.matcher(description);
+        if (!m.matches()) {
+            throw new IllegalArgumentException(description + " cannot be parsed");
+        }
         this.category = m.group(1);
         this.blurb = m.group(2);
         this.price = m.group(3);
         this.url = m.group(4);
-
     }
 
-    public String getCategory() { return category; }
-    public String getBlurb() { return blurb; }
-    public String getPrice() { return price; }
-    public String getUrl() { return url; }
+    String getCategory() {
+        return category;
+    }
+
+    String getBlurb() {
+        return blurb;
+    }
+
+    String getPrice() {
+        return price;
+    }
+
+    String getUrl() {
+        return url;
+    }
+
+    String getId() {
+        return id;
+    }
+
+    String getTitle() {
+        return title;
+    }
 }
--- src/test/unit/java/org/corvine/demo/domain/ItemTest.java
+++ src/test/unit/java/org/corvine/demo/domain/ItemTest.java
@@ -7,11 +7,19 @@ import static org.junit.Assert.assertEquals;
 public class ItemTest {
     @Test
     public void shouldBeAbleToParseDescriptionFieldsOnConstruction() {
-        Item item = new Item("title", "<p>Filed under: Solid State Combos</p><p>Made japan. Vintage 60 watt 1x12\" with overdrive & reverb!<br />Price: $380.00<a href=\"http://www.musicswopshop.com.au/stock?search=MSS 56874\"><br />Read more ...</a></p>", "link");
+        Item item = new Item("MSS 56918 | VOX V847-A", "<p>Filed under: Solid State Combos</p><p>Made japan. Vintage 60 watt 1x12\" with overdrive & reverb!<br />Price: $380.00<a href=\"http://www.musicswopshop.com.au/stock?search=MSS 56874\"><br />Read more ...</a></p>", "link");
 
         assertEquals("Solid State Combos", item.getCategory());
         assertEquals("Made japan. Vintage 60 watt 1x12\" with overdrive & reverb!", item.getBlurb());
         assertEquals("$380.00", item.getPrice());
         assertEquals("http://www.musicswopshop.com.au/stock?search=MSS 56874", item.getUrl());
+    }
+
+    @Test
+    public void shouldBeAbleToParseTitleFieldsOnConstruction() {
+        Item item = new Item("MSS 56918 | VOX V847-A", "<p>Filed under: Solid State Combos</p><p>Made japan. Vintage 60 watt 1x12\" with overdrive & reverb!<br />Price: $380.00<a href=\"http://www.musicswopshop.com.au/stock?search=MSS 56874\"><br />Read more ...</a></p>", "link");
+
+        assertEquals("MSS 56918", item.getId());
+        assertEquals("VOX V847-A", item.getTitle());
     }
 }
