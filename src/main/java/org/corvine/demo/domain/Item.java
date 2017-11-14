192efc4456a07a6c9f37437f38d9964dd8aa1571
Andy Marks <vampwillow@gmail.com>
2017-11-14 21:49:01 +1100

    Starting to test parsing of embedded HTML structure in feed description

--- src/main/java/org/corvine/demo/domain/Item.java
+++ src/main/java/org/corvine/demo/domain/Item.java
@@ -2,11 +2,18 @@ package org.corvine.demo.domain;
 
 import lombok.Data;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Data
 public class Item {
     private String title;
     private String description;
     private String link;
+    private String category;
+    private String blurb;
+    private String price;
+    private String url;
 
     private Item() {
     }
@@ -16,5 +23,18 @@ public class Item {
         this.title = title;
         this.description = description;
         this.link = link;
+
+        Pattern p = Pattern.compile("<p>Filed under: (.*)</p><p>(.*)<br />Price: (.*)<a href=\"(.*)\"><br />Read more ...</a></p>");
+        Matcher m = p.matcher(description);
+        this.category = m.group(1);
+        this.blurb = m.group(2);
+        this.price = m.group(3);
+        this.url = m.group(4);
+
     }
+
+    public String getCategory() { return category; }
+    public String getBlurb() { return blurb; }
+    public String getPrice() { return price; }
+    public String getUrl() { return url; }
 }
---
+++ src/test/unit/java/org/corvine/demo/domain/ItemTest.java
@@ -0,0 +1,17 @@
+package org.corvine.demo.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ItemTest {
+    @Test
+    public void shouldBeAbleToParseDescriptionFieldsOnConstruction() {
+        Item item = new Item("title", "<p>Filed under: Solid State Combos</p><p>Made japan. Vintage 60 watt 1x12\" with overdrive & reverb!<br />Price: $380.00<a href=\"http://www.musicswopshop.com.au/stock?search=MSS 56874\"><br />Read more ...</a></p>", "link");
+
+        assertEquals("Solid State Combos", item.getCategory());
+        assertEquals("Made japan. Vintage 60 watt 1x12\" with overdrive & reverb!", item.getBlurb());
+        assertEquals("$380.00", item.getPrice());
+        assertEquals("http://www.musicswopshop.com.au/stock?search=MSS 56874", item.getUrl());
+    }
+}
