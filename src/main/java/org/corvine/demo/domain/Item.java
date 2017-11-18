525f9b39c753b96d29d1faf999abf31e2e60652e
Andy Marks <vampwillow@gmail.com>
2017-11-18 21:30:09 +1100

    Tightened up regex matching on description to handle more variations.

--- src/main/java/org/corvine/demo/domain/Item.java
+++ src/main/java/org/corvine/demo/domain/Item.java
@@ -28,22 +28,44 @@ public class Item {
         Pattern titleRE = Pattern.compile("(MSS\\s[\\d]*)\\s\\|\\s(.*)");
         Matcher m = titleRE.matcher(title);
         if (!m.matches()) {
-            throw new IllegalArgumentException("Title: " + title + " cannot be parsed");
+            throw new IllegalArgumentException("Title: [" + title + "] cannot be parsed");
         }
         this.title = m.group(2);
         this.id = m.group(1);
     }
 
     private void parseDescriptionComponents(String description) {
-        Pattern descriptionRE = Pattern.compile("<p>Filed under: (.*)<\\/p><p>(.*)<br \\/>Price: (.*)<a href=\"(.*)\"><br \\/>Read more ...<\\/a><\\/p>");
+        Pattern descriptionRE = Pattern.compile("\\s*<p>Filed under: (.*)<\\/p><p>(.*)<br \\/>Price: (.*)<a href=\"(.*)\"><br \\/>Read more ...<\\/a><\\/p>\\s*");
         Matcher m = descriptionRE.matcher(description);
-        if (!m.matches()) {
-            throw new IllegalArgumentException("Description: " + description + " cannot be parsed");
+        this.category = extractCategory(description);
+        this.blurb = extractBlurb(description);
+        this.price = extractPrice(description);
+        this.url = extractUrl(description);
+    }
+
+    private String extractField(String description, String fieldSpec, String fieldTitle) {
+        Pattern pattern = Pattern.compile(fieldSpec, Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(description);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(fieldTitle + " in [" + description + "] cannot be parsed");
         }
-        this.category = m.group(1);
-        this.blurb = m.group(2);
-        this.price = m.group(3);
-        this.url = m.group(4);
+        return matcher.group(1);
+    }
+
+    private String extractBlurb(String description) {
+        return extractField(description, "\\s*<p>Filed under: .*<\\/p><p>(.*)<br \\/>Price.*", "Blurb");
+    }
+
+    private String extractUrl(String description) {
+        return extractField(description, ".*<a href=\"(.*)\">.*", "Uri");
+    }
+
+    private String extractPrice(String description) {
+        return extractField(description, ".*<br \\/>Price: (.*)<a href=\".*", "Price");
+    }
+
+    private String extractCategory(String description) {
+        return extractField(description, "\\s*<p>Filed under: (\\p{Print}*)<\\/p><p>.*", "Category");
     }
 
     String getCategory() {
--- src/test/unit/java/org/corvine/demo/domain/ItemTest.java
+++ src/test/unit/java/org/corvine/demo/domain/ItemTest.java
@@ -6,26 +6,53 @@ import static org.junit.Assert.assertEquals;
 
 public class ItemTest {
     @Test
-    public void shouldBeAbleToParseDescriptionFieldsOnConstruction() {
+    public void parsesDescriptionFieldsSuccessfully() {
         Item item = new Item("MSS 56918 | VOX V847-A", "<p>Filed under: Solid State Combos</p><p>Made japan. Vintage 60 watt 1x12\" with overdrive & reverb!<br />Price: $380.00<a href=\"http://www.musicswopshop.com.au/stock?search=MSS 56874\"><br />Read more ...</a></p>", "link");
 
         assertEquals("Solid State Combos", item.getCategory());
-        assertEquals("Made japan. Vintage 60 watt 1x12\" with overdrive & reverb!", item.getBlurb());
         assertEquals("$380.00", item.getPrice());
         assertEquals("http://www.musicswopshop.com.au/stock?search=MSS 56874", item.getUrl());
+        assertEquals("Made japan. Vintage 60 watt 1x12\" with overdrive & reverb!", item.getBlurb());
     }
+
     @Test
-    public void shouldBeAbleToParseLongerDescriptionFieldsOnConstruction() {
-        Item item = new Item("MSS 56918 | VOX V847-A", "<p>Filed under: Stomp Boxes</p><p>Voltage Doubled FET Preamp. Dual FETs for the most tube like tone in a transistor design. <br> With the speed knob Pushed In, think of it as a volume control that goes to 20. <br> With the speed knob Pulled Out, it’s a cutting boost with a bit of gain. Not quite an overdrive, just overdriven tube grit and edge to your tone.<br> Includes box<br />Price: $225.00<a href=\"http://www.musicswopshop.com.au/stock?search=MSS 56958\"><br />Read more ...</a></p>", "link");
+    public void parsesBlurbsWithCarriageReturns() {
+        Item item = new Item("MSS 56918 | VOX V847-A", "<p>Filed under: Stomp Boxes</p><p>Voltage Doubled FET Preamp. Dual FETs for the most tube like tone in a transistor design. <br>\n" +
+                "With the speed knob Pushed In, think of it as a volume control that goes to 20. <br>\n" +
+                "With the speed knob Pulled Out, it’s a cutting boost with a bit of gain. Not quite an overdrive, just overdriven tube grit and edge to your tone.<br>\n" +
+                "Includes box<br />Price: $225.00<a href=\"http://www.musicswopshop.com.au/stock?search=MSS 56958\"><br />Read more ...</a></p> ", "link");
 
         assertEquals("Stomp Boxes", item.getCategory());
-        assertEquals("Voltage Doubled FET Preamp. Dual FETs for the most tube like tone in a transistor design. <br> With the speed knob Pushed In, think of it as a volume control that goes to 20. <br> With the speed knob Pulled Out, it’s a cutting boost with a bit of gain. Not quite an overdrive, just overdriven tube grit and edge to your tone.<br> Includes box", item.getBlurb());
+        assertEquals("Voltage Doubled FET Preamp. Dual FETs for the most tube like tone in a transistor design. <br>\n" +
+                "With the speed knob Pushed In, think of it as a volume control that goes to 20. <br>\n" +
+                "With the speed knob Pulled Out, it’s a cutting boost with a bit of gain. Not quite an overdrive, just overdriven tube grit and edge to your tone.<br>\n" +
+                "Includes box", item.getBlurb());
         assertEquals("$225.00", item.getPrice());
         assertEquals("http://www.musicswopshop.com.au/stock?search=MSS 56958", item.getUrl());
     }
 
     @Test
-    public void shouldBeAbleToParseTitleFieldsOnConstruction() {
+    public void parsesBlurbsWithPunctuationInCategory() {
+        Item item = new Item("MSS 56918 | VOX V847-A", "<p>Filed under: Keyboards, Synths & Controllers</p><p>88 fully weighted electric piano. <br> SuperNATURAL Piano engine, an all-new PHA III Ivory Feel-S Keyboard with Escapement, an onboard Looper, and a mic input with companion harmony effects.<br> On board speakers! USB connectivity. <br> Includes power supply, Roland sustain pedal, double-braced stand and Gator gig bag<br />Price: $1,450.00<a href=\"http://www.musicswopshop.com.au/stock?search=MSS 56994\"><br />Read more ...</a></p>", "link");
+
+        assertEquals("Keyboards, Synths & Controllers", item.getCategory());
+        assertEquals("88 fully weighted electric piano. <br> SuperNATURAL Piano engine, an all-new PHA III Ivory Feel-S Keyboard with Escapement, an onboard Looper, and a mic input with companion harmony effects.<br> On board speakers! USB connectivity. <br> Includes power supply, Roland sustain pedal, double-braced stand and Gator gig bag", item.getBlurb());
+        assertEquals("$1,450.00", item.getPrice());
+        assertEquals("http://www.musicswopshop.com.au/stock?search=MSS 56994", item.getUrl());
+    }
+
+    @Test
+    public void parsesLongerDescriptionFieldsSuccessfully() {
+        Item item = new Item("MSS 56918 | VOX V847-A", "<p>Filed under: Stomp Boxes</p><p>Voltage Doubled FET Preamp. Dual FETs for the most tube like tone in a transistor design. <br>With the speed knob Pushed In, think of it as a volume control that goes to 20. <br>With the speed knob Pulled Out, it’s a cutting boost with a bit of gain. Not quite an overdrive, just overdriven tube grit and edge to your tone.<br>Includes box<br />Price: $225.00<a href=\"http://www.musicswopshop.com.au/stock?search=MSS 56958\"><br />Read more ...</a></p> ", "link");
+
+        assertEquals("Stomp Boxes", item.getCategory());
+        assertEquals("$225.00", item.getPrice());
+        assertEquals("http://www.musicswopshop.com.au/stock?search=MSS 56958", item.getUrl());
+        assertEquals("Voltage Doubled FET Preamp. Dual FETs for the most tube like tone in a transistor design. <br>With the speed knob Pushed In, think of it as a volume control that goes to 20. <br>With the speed knob Pulled Out, it’s a cutting boost with a bit of gain. Not quite an overdrive, just overdriven tube grit and edge to your tone.<br>Includes box", item.getBlurb());
+    }
+
+    @Test
+    public void parsesTitleFieldsSuccessfully() {
         Item item = new Item("MSS 56918 | VOX V847-A", "<p>Filed under: Solid State Combos</p><p>Made japan. Vintage 60 watt 1x12\" with overdrive & reverb!<br />Price: $380.00<a href=\"http://www.musicswopshop.com.au/stock?search=MSS 56874\"><br />Read more ...</a></p>", "link");
 
         assertEquals("MSS 56918", item.getId());
