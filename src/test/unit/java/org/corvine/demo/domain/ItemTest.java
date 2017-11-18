16d3f2679a0f97111dc0e21121779ac1c68e1ac8
Andy Marks <vampwillow@gmail.com>
2017-11-19 07:16:49 +1100

    I'd rather call out negative tests by name and have positive tests as the default assumption

--- src/test/unit/java/org/corvine/demo/domain/ItemTest.java
+++ src/test/unit/java/org/corvine/demo/domain/ItemTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 
 public class ItemTest {
     @Test
-    public void parsesDescriptionFieldsSuccessfully() {
+    public void parsesDescriptionFields() {
         Item item = new Item("MSS 56918 | VOX V847-A", "<p>Filed under: Solid State Combos</p><p>Made japan. Vintage 60 watt 1x12\" with overdrive & reverb!<br />Price: $380.00<a href=\"http://www.musicswopshop.com.au/stock?search=MSS 56874\"><br />Read more ...</a></p>", "link");
 
         assertEquals("Solid State Combos", item.getCategory());
@@ -42,7 +42,7 @@ public class ItemTest {
     }
 
     @Test
-    public void parsesLongerDescriptionFieldsSuccessfully() {
+    public void parsesLongerDescriptionFields() {
         Item item = new Item("MSS 56918 | VOX V847-A", "<p>Filed under: Stomp Boxes</p><p>Voltage Doubled FET Preamp. Dual FETs for the most tube like tone in a transistor design. <br>With the speed knob Pushed In, think of it as a volume control that goes to 20. <br>With the speed knob Pulled Out, it’s a cutting boost with a bit of gain. Not quite an overdrive, just overdriven tube grit and edge to your tone.<br>Includes box<br />Price: $225.00<a href=\"http://www.musicswopshop.com.au/stock?search=MSS 56958\"><br />Read more ...</a></p> ", "link");
 
         assertEquals("Stomp Boxes", item.getCategory());
@@ -52,7 +52,7 @@ public class ItemTest {
     }
 
     @Test
-    public void parsesTitleFieldsSuccessfully() {
+    public void parsesTitleFields() {
         Item item = new Item("MSS 56918 | VOX V847-A", "<p>Filed under: Solid State Combos</p><p>Made japan. Vintage 60 watt 1x12\" with overdrive & reverb!<br />Price: $380.00<a href=\"http://www.musicswopshop.com.au/stock?search=MSS 56874\"><br />Read more ...</a></p>", "link");
 
         assertEquals("MSS 56918", item.getId());
