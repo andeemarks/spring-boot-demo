301a6d5cc7089f36f91800a260512324fd1587da
Andy Marks <vampwillow@gmail.com>
2017-11-26 08:16:07 +1100

    Now displaying item ID for help with later navigation

--- src/test/integration/java/org/corvine/demo/repository/FeedRepositoryIntegrationTest.java
+++ src/test/integration/java/org/corvine/demo/repository/FeedRepositoryIntegrationTest.java
@@ -1,13 +1,11 @@
 package org.corvine.demo.repository;
 
-import org.corvine.demo.domain.Item;
+import org.corvine.demo.domain.RecentItemList;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
@@ -20,7 +18,7 @@ public class FeedRepositoryIntegrationTest {
 
     @Test
     public void feedListShouldShowItems() throws Exception {
-        List<Item> items = repository.findAllItems();
-        assertTrue("Not enough items found: " + items.size(), items.size() > 0);
+        RecentItemList list = repository.findAllItems();
+        assertTrue("Not enough items found: " + list.getItems().size(), list.getItems().size() > 0);
     }
 }
